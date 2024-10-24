4d2d9f66e098bba315063684bc3a3f411890a5b0
dineshadari <142793248+dineshadari@users.noreply.github.com>
2024-10-24 11:43:34 +0530

    Update Step1.java

--- jdbc2/src/jdbc2/Step1.java
+++ jdbc2/src/jdbc2/Step1.java
@@ -5,6 +5,7 @@ public class Step1
 	public static void main(String[] args) throws ClassNotFoundException 
 	{
 		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Successfull");
 
 	}
 }
